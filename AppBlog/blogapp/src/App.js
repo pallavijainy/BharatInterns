9014c75bbe4ec2e3bccf6e79b9998aff627d4f9e
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-02 02:00:49 +0530

    check drag drop

--- AppBlog/blogapp/src/App.js
+++ AppBlog/blogapp/src/App.js
@@ -1,12 +1,14 @@
 import RouterPage from "./AllRoutes/RouterPage";
 import "./App.css";
+import Drag from "./component/Drag";
 import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <RouterPage />
+      {/* <Navbar />
+      <RouterPage /> */}
+      <Drag></Drag>
     </>
   );
 }
