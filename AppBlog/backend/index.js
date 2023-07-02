eb9ba86854157f34cb39749d9e3e59a32274ef2d
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-03 02:59:48 +0530

    use multer library to add the image

--- AppBlog/backend/index.js
+++ AppBlog/backend/index.js
@@ -5,9 +5,24 @@ const bcrypt = require("bcrypt"); //for bcrypting the password
 const jwt = require("jsonwebtoken"); //for send the token
 const cors = require("cors"); // to add frontend to backend
 const { blogModel } = require("./Model/BlogModel");
+const { upload } = require("./Middleware/Upload");
+const Grid = require("gridfs-stream"); //for image upload
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+let gfs, gridfsBucket;
+const conn = mongoose.connection;
+conn.once("open", () => {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "photos",
+  });
+
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("photos");
+  // all set!
+});
 
 //userRouter
 app.post("/register", async (req, res) => {
@@ -62,6 +77,28 @@ app.post("/addblog", async (req, res) => {
     res.send({ msg: "Blog created successfully" });
   } catch (error) {
     res.send({ msg: "something went wrong" });
+  }
+});
+
+app.post("/file/upload", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      res.status({ msg: "file not found" });
+    }
+    const imageurl = `http://localhost:8000/file/${req.file.filename}`;
+    res.send(imageurl);
+  } catch (error) {
+    res.send({ msg: "something went wrong" });
+  }
+});
+
+app.get("/file/:filename", async (req, res) => {
+  try {
+    const file = await gfs.files.findOne({ filename: req.params.filename });
+    const readStream = gridfsBucket.openDownloadStream(file._id);
+    readStream.pipe(res);
+  } catch (error) {
+    res.send({ msg: error.message });
   }
 });
 
