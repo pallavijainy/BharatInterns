982dfe51629e1ea15c2df92ddd50f8112a22757b
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-10 00:58:16 +0530

    add frontend

--- Video Conferencing app/frontend/chatapp/src/AllRoute/RouterPage.jsx
+++ Video Conferencing app/frontend/chatapp/src/AllRoute/RouterPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../component/Home";
+
 import Login from "../component/Login";
 
 import Singup from "../component/Singup";
@@ -9,7 +9,7 @@ import Dashboard from "./../component/Dashboard";
 const RouterPage = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Dashboard />}></Route>
       <Route path="/signup" element={<Singup />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
--- Video Conferencing app/frontend/chatapp/src/component/Dashboard.jsx
+++ Video Conferencing app/frontend/chatapp/src/component/Dashboard.jsx
@@ -1,75 +1,123 @@
-import React, { useEffect } from "react";
-import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import "./Dashboard.css";
 
-import Swal from "sweetalert2";
-
-import Footer from "./Footer";
-import { useNavigate } from "react-router-dom";
-
-// Import any additional CSS files here
-
-const Dashboard = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    userLoggedIn();
-  }, []);
-
-  const userLoggedIn = async () => {
-    const token = localStorage.getItem("token");
-    const request = await fetch("http://localhost:8080/user/check", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ token: token }),
-    });
-
-    const response = await request.json();
-    if (!response.ok) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Please Login First",
-      });
-      setTimeout(() => {
-        navigate("/login");
-      }, 3000);
-    }
-  };
-
-  const handleLogout = async () => {
-    const token = localStorage.getItem("token");
-
-    const request = await fetch("http://localhost:8080/user/logout", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ token }),
-    });
-
-    localStorage.clear();
-    localStorage.setItem("signedIn", false);
-    navigate("/login");
-  };
-
+const MainDashboard = () => {
   return (
-    <>
-      <Box id="main_dashboard">
-        <Box id="ïmg1">
+    <Box id="main_dashboard">
+      <Box id="ïmg1">
+        <Image
+          className="img1_main"
+          src="https://user-images.githubusercontent.com/115463536/229345353-f12c3416-fa09-4d7c-8190-a42273105e9a.gif"
+          alt=""
+        />
+      </Box>
+      <Box
+        id="main_img_info"
+        display="grid"
+        gridTemplateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+        gap="4"
+        marginTop="5%"
+      >
+        <a href="https://talkies-video-chat.netlify.app?type=videochat">
+          <Box
+            className="div_img_info"
+            textAlign="center"
+            textDecoration="none"
+            color="black"
+            borderRadius="10%"
+            boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
+            transition="transform 0.2s"
+            _hover={{ transform: "scale(1.1)" }}
+            height={{ base: "300px", md: "70%" }}
+          >
+            <Image
+              className="img_info"
+              src="https://user-images.githubusercontent.com/115463536/229345389-d7c5186e-d34c-46fa-a5e7-3b63815f9305.gif"
+              alt=""
+              height={{ base: "75%", md: "100%" }}
+              borderRadius="10%"
+            />
+            <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
+              Video Call
+            </Text>
+          </Box>
+        </a>
+        <a href="https://talkies-share-screen.netlify.app?type=screenshare">
+          <Box
+            className="div_img_info"
+            textAlign="center"
+            textDecoration="none"
+            color="black"
+            borderRadius="10%"
+            boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
+            transition="transform 0.2s"
+            _hover={{ transform: "scale(1.1)" }}
+            height={{ base: "300px", md: "70%" }}
+          >
+            <Image
+              className="img_info"
+              src="https://user-images.githubusercontent.com/115463536/229345427-8a022b0e-2638-4ce3-b4e2-ffc94ba79745.gif"
+              alt=""
+              height={{ base: "75%", md: "100%" }}
+              borderRadius="10%"
+            />
+            <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
+              Screen Share
+            </Text>
+          </Box>
+        </a>
+        <a href="https://talkies-lets-connect.netlify.app/room.html?type=message">
+          <Box
+            className="div_img_info"
+            textAlign="center"
+            textDecoration="none"
+            color="black"
+            borderRadius="10%"
+            boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
+            transition="transform 0.2s"
+            _hover={{ transform: "scale(1.1)" }}
+            height={{ base: "300px", md: "70%" }}
+          >
+            <Image
+              className="img_info"
+              src="https://user-images.githubusercontent.com/115463536/229345461-fd213545-2b3c-4ae0-93f3-ef1f5e725eff.gif"
+              alt=""
+              height={{ base: "75%", md: "100%" }}
+              borderRadius="10%"
+            />
+            <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
+              Message
+            </Text>
+          </Box>
+        </a>
+        <Box
+          as="span"
+          id="logout"
+          className="div_img_info"
+          textAlign="center"
+          textDecoration="none"
+          color="black"
+          borderRadius="10%"
+          boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
+          transition="transform 0.2s"
+          _hover={{ transform: "scale(1.1)" }}
+          height={{ base: "300px", md: "70%" }}
+        >
           <Image
-            className="img1_main"
-            src="https://user-images.githubusercontent.com/115463536/229345353-f12c3416-fa09-4d7c-8190-a42273105e9a.gif"
+            className="img_info"
+            src="https://user-images.githubusercontent.com/115463536/229345490-98dd7120-789b-4411-899d-4a8ad9c84b5c.gif"
             alt=""
+            height={{ base: "75%", md: "100%" }}
+            borderRadius="10%"
           />
-        </Box>
-        <Box id="main_img_info">
-          {/* Add your dashboard image components */}
+          <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
+            Logout
+          </Text>
         </Box>
       </Box>
-      <Footer />
-    </>
+    </Box>
   );
 };
 
-export default Dashboard;
+export default MainDashboard;
