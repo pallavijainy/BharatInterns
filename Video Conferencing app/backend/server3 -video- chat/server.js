09d9f6cd1bf7166644c78b9f5f725662da418a2e
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-10 16:33:22 +0530

    add backend

---
+++ Video Conferencing app/backend/server3 -video- chat/server.js
@@ -0,0 +1,56 @@
+// require("dotenv").config({ path: "/Users/owaisazmi/Desktop/melted-group-7444/Backend/.env" });
+require("dotenv").config();
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const { roomRoute } = require("./routes/room.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const httpServer = http.createServer(app);
+
+app.get("/start", async (req, res) => {
+  // Using setitemout so that the connection should be established
+  try {
+    setTimeout(() => {
+      res.send({ ok: true, msg: "Connection Established successfully" });
+    }, 3000);
+  } catch (error) {
+    res.send({ ok: false, msg: "Something went wrong" });
+  }
+});
+
+app.use("/room", roomRoute);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (RoomID, userID) => {
+    console.log(`${userID} joined room ${RoomID}`);
+    socket.join(RoomID);
+    socket.to(RoomID).emit("user-join", userID);
+
+    socket.on("disconnect", () => {
+      socket.to(RoomID).emit("user-disconnected", userID);
+    });
+  });
+});
+
+const PORT = 3000; // Set your desired port here
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
