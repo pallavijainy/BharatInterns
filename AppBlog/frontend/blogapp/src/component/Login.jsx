7df0a16d97fc0c1a376e97f8ef4015df39db4513
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-10 18:50:22 +0530

    add login

--- AppBlog/frontend/blogapp/src/component/Login.jsx
+++ AppBlog/frontend/blogapp/src/component/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
+import image from "../images/Popular-social-media-Blogger-logo-on-transparent-PNG.png";
 import {
   Flex,
   Box,
@@ -12,16 +13,17 @@ import {
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+  Image,
+} from "@chakra-ui/react";
 
-import { useForm } from 'react-hook-form';
-import axios from 'axios'
-import{Navigate} from 'react-router-dom'
-import { UserContext } from '../Context/UserContext';
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
 
 const Login = () => {
-  const {setUserInfo} = useContext(UserContext)
-  const[redirect,setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(false);
   const {
     register,
     handleSubmit,
@@ -30,99 +32,102 @@ const Login = () => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data,"-------->data")
-   axios.post("http://localhost:8000/login", data,{
-  withCredentials: true
-})
-   .then((res)=> {
-     
- console.log(res.data?.email)
-     if(res.data?.email){
-               setUserInfo({email:res.data.email})
-               console.log(res.data.email)
-              setRedirect(true);
-          }
-           alert(res.data.msg);
-           
-          })
-        
-   .catch((errors)=>{console.log(errors)})
+    console.log(data, "-------->data");
+    axios
+      .post("http://localhost:8000/login", data, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data?.email);
+        if (res.data?.email) {
+          setUserInfo({ email: res.data.email, id: res.data.id });
+          console.log(res.data.email);
+          setRedirect(true);
+        }
+        alert(res.data.msg);
+      })
+
+      .catch((errors) => {
+        console.log(errors);
+      });
   };
 
-
-  if(redirect){
-    return (<Navigate to={'/'}/>)
+  if (redirect) {
+    return <Navigate to={"/showpost"} />;
   }
   return (
     <div>
-         <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          // bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input  
-              {...register('email', {
-                    required: 'Email must be required',
+      <Flex minH={"100vh"} align={"center"} justify={"center"}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Image src={image} width={"100px"} />
+            <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              to enjoy all of our cool features ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            // bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  {...register("email", {
+                    required: "Email must be required",
                     pattern: {
                       value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
                       message: "Email doesn't exist",
                     },
                   })}
-              type="email" />
-             
-            </FormControl>
-             {errors.email && (<Text color="red"> {errors.email.message} </Text>)}
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password"
-               {...register('password', {
-                      required:"paawoed must be required"})}
-              
-              />
-            </FormControl>
+                  type="email"
+                />
+              </FormControl>
+              {errors.email && (
+                <Text color="red"> {errors.email.message} </Text>
+              )}
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  {...register("password", {
+                    required: "paawoed must be required",
+                  })}
+                />
+              </FormControl>
 
-             {errors.password && (<Text color="red"> {errors.password.message} </Text>)}
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
+              {errors.password && (
+                <Text color="red"> {errors.password.message} </Text>
+              )}
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={"blue.400"}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  onClick={handleSubmit(onSubmit)}
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Sign in
+                </Button>
               </Stack>
-              <Button
-               onClick={handleSubmit(onSubmit)}
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-
-                }}>
-                  
-                Sign in
-              </Button>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Box>
+        </Stack>
+      </Flex>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- AppBlog/frontend/blogapp/src/component/Post.jsx
+++ AppBlog/frontend/blogapp/src/component/Post.jsx
@@ -1,34 +1,59 @@
-import { Box, Image, Text, Heading, Stack,Flex } from '@chakra-ui/react';
-import { formatISO9075 } from 'date-fns'
-import { Link } from 'react-router-dom';
-const  Post = ({_id, title, cover, content, summary,createdAt,author }) => {
-    console.log(title, cover, content, summary,"pal")
-    return (
-       <Box boxShadow="md" p={4} rounded="md">
-      <Flex>
-        <Link to={`/post/${_id}`}>
-        <Box width={500} height={400} mr={4}>
-          <Image src={'http://localhost:8000/' + cover} alt={title} objectFit="cover" width="100%" height="100%" />
-        </Box>
-        </Link>
-        <Box>
-          <Heading as="h2" size="lg" mb={2}>
-            {title}
-          </Heading>
-          <Text>
-            {/* <a>{`${author.firstname} ${author.lastname}`}</a> */}
-   <time>{formatISO9075(new Date(createdAt))}</time>
-          </Text>
-          <Text fontSize="md" color="gray.600" mb={4}>
-            {summary}
-          </Text>
-          <Text fontSize="sm" color="gray.500">
-            {content}
-          </Text>
-        </Box>
-      </Flex>
-    </Box>
+import { Box, Image, Text, Heading, Stack, Flex } from "@chakra-ui/react";
+import { formatISO9075 } from "date-fns";
+import { Link } from "react-router-dom";
+const Post = ({ _id, title, cover, content, summery, createdAt, author }) => {
+  console.log(title, cover, content, summery, "pal");
+  return (
+    <Link to={`/post/${_id}`}>
+      <Box
+        m={"auto"}
+        width={"90%"}
+        height={"10%"}
+        shadow={"base"}
+        textAlign={"left"}
+        mt={20}
+        rounded="md"
+      >
+        <Flex>
+          <Link to={`/post/${_id}`}>
+            <Box width={500} mr={4}>
+              <Image
+                src={"http://localhost:8000/" + cover}
+                alt={title}
+                objectFit="cover"
+                width="100%"
+                height="50%"
+              />
+            </Box>
+          </Link>
 
+          <Box>
+            <Heading mt={5} textAlign={"left"} as="h2" size="lg" mb={2}>
+              {title}
+            </Heading>
+            <Text
+              fontSize={"sm"}
+              fontWeight={"medium"}
+              color={"blackAlpha.700"}
+            >
+              {`${author.firstname} `}
+              <time style={{ color: "#A9A9A9", fontSize: "12px" }}>
+                {formatISO9075(new Date(createdAt))}
+              </time>
+            </Text>
+            <Text
+              mt={5}
+              fontSize="lg"
+              fontWeight={"medium"}
+              color={"blackAlpha.700"}
+              pr={10}
+            >
+              {summery}
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Link>
   );
 };
 
