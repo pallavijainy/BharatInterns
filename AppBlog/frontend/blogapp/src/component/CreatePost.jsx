f2691b1f9a9d5f612662afb1818235529e37e3a9
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-10 18:53:24 +0530

    create

--- AppBlog/frontend/blogapp/src/component/CreatePost.jsx
+++ AppBlog/frontend/blogapp/src/component/CreatePost.jsx
@@ -1,43 +1,61 @@
-import React, { useState } from 'react'
-import { Box, FormControl, FormLabel, Input, Button, Select, Image } from "@chakra-ui/react";
-import ReactQuil from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import axios from 'axios';
-import {Navigate} from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Select,
+  Image,
+} from "@chakra-ui/react";
+import ReactQuil from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
-
-    const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
     ],
-  };
-   
-   const   formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-  ]
+    ["link", "image"],
+    ["video"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+];
 const CreatePost = () => {
-
-    const [selectedOption, setSelectedOption] = useState("blog");
-    const[title,setTitle] = useState('');
-    const[summery,setSummery] = useState('');
-    const[content,setContent] = useState('');
-    const[file,setFile] = useState('');
-    const[redirect,setRedirect] = useState(false);
-
-
+  const [selectedOption, setSelectedOption] = useState("blog");
+  const [title, setTitle] = useState("");
+  const [summery, setSummery] = useState("");
+  const [content, setContent] = useState("");
+  const [file, setFile] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
-    const getImageUrl = () => {
+  const getImageUrl = () => {
     // Define image URLs based on selected option
     switch (selectedOption) {
       case "fitness":
@@ -55,70 +73,87 @@ const CreatePost = () => {
     }
   };
 
-  const createNewPost = async (e)=>{
-     console.log(file,'==>file')
+  const createNewPost = async (e) => {
+    console.log(file, "==>file");
 
-     const data = new FormData();
-     data.set('title',title);
-     data.set('summery',summery);
-     data.set('content',content);
-     data.set('file',file[0]);
-    
-e.preventDefault();
-     await axios.post('http://localhost:8000/createpost', data,{
-       withCredentials:true
-     }
-           
-     ).then(()=> alert('Created post Sucessfully'))
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summery", summery);
+    data.set("content", content);
+    data.set("file", file[0]);
+
+    e.preventDefault();
+    await axios
+      .post("http://localhost:8000/createpost", data, {
+        withCredentials: true,
+      })
+      .then(() => alert("Created post Sucessfully"))
       .then(() => setRedirect(true))
-       .catch((err)=> alert('Something went Wrong'))
+      .catch((err) => alert("Something went Wrong"));
+  };
+
+  if (redirect) {
+    return <Navigate to={"/showpost"} />;
   }
-  
-   if(redirect){
-     return(<Navigate to={'/showpost'}/>)
-   }
   return (
-   <>
-   {selectedOption && (
-        <Box  m={2} height={'full'}  mt={4}>
-          <Image height={'600'} width={'full'} src={getImageUrl()} alt={selectedOption} />
+    <>
+      {selectedOption && (
+        <Box m={2} height={"full"} mt={4}>
+          <Image
+            height={"600"}
+            width={"full"}
+            src={getImageUrl()}
+            alt={selectedOption}
+          />
         </Box>
       )}
-    <Box m={'auto'} mt={10} width={'70%'}>
+      <Box m={"auto"} mt={10} width={"70%"}>
+        <Box>
+          <FormLabel>Select Category</FormLabel>
+          <Select value={selectedOption} onChange={handleOptionChange}>
+            <option value="fitness">Fitness</option>
+            <option value="crime">Crime</option>
+            <option value="political">Political</option>
+            <option value="sport">Sport</option>
+            <option value="worldwide">Worldwide</option>
+          </Select>
+        </Box>
+        <form onSubmit={createNewPost}>
+          <FormControl>
+            <FormLabel>Title</FormLabel>
+            <Input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel>Summary</FormLabel>
+            <Input
+              type="text"
+              placeholder="Summary"
+              value={summery}
+              onChange={(e) => setSummery(e.target.value)}
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel>File</FormLabel>
+            <Input type="file" onChange={(e) => setFile(e.target.files)} />
+          </FormControl>
+          <ReactQuil
+            value={content}
+            modules={modules}
+            formats={formats}
+            onChange={(newValue) => setContent(newValue)}
+          />
+          <Button width={"full"} mt={7} colorScheme="gray" type="submit">
+            Create post
+          </Button>
+        </form>
+      </Box>
+    </>
+  );
+};
 
-          <Box >
-             <FormLabel>Select Category</FormLabel>
-            <Select value={selectedOption} onChange={handleOptionChange}>
-        <option value="fitness">Fitness</option>
-        <option value="crime">Crime</option>
-        <option value="political">Political</option>
-        <option value="sport">Sport</option>
-        <option value="worldwide">Worldwide</option>
-      </Select>
-     
-     
-    </Box>
-      <form onSubmit={createNewPost}>
-        <FormControl>
-          <FormLabel>Title</FormLabel>
-          <Input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)}/>
-        </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>Summary</FormLabel>
-          <Input type="text" placeholder="Summary" value={summery} onChange={e => setSummery(e.target.value)} />
-        </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>File</FormLabel>
-          <Input type="file" onChange={e => setFile(e.target.files)} />
-        </FormControl>
-        <ReactQuil value={content} modules={modules} formats={formats}  onChange={newValue => setContent(newValue)}/>
-        <Button width={'full'} mt={7} colorScheme="gray" type="submit">
-          Create post
-        </Button>
-      </form>
-    </Box>
-     </> 
-  )
-}
-
-export default CreatePost
+export default CreatePost;
